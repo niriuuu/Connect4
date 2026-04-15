80142bf60ed0a83b96f735043b4288e7654cba84
niriu <oroke3@gmail.com>
2026-04-15 18:08:32 -0400

    install sfml-cpp and implemented basic window loop in main

--- main.cpp
+++ main.cpp
@@ -1,10 +1,52 @@
-#include<iostream>
+#include <SFML/Graphics.hpp>
 
-using namespace std;
+using namespace sf; // Namespace pour les objets de la librairie SFML
+using namespace std; // Namespace pour les objets de la librairie standard en C++
 
-int main()
-{
-	cout << "wow";
-	cout << "waw";
-	system("pause>0");
+int main() {
+	RenderWindow window(VideoMode(800, 600), "My window");
+
+	RectangleShape background;
+	Color backgroundColor(Uint8(35), Uint8(75), Uint8(20), Uint8(255));
+
+	RectangleShape grid;
+
+	CircleShape token;
+	int tokenSize = 64;
+
+	
+
+	window.setFramerateLimit(60);
+	
+	background.setSize(Vector2f(800, 600));
+	background.setFillColor(backgroundColor);
+
+	grid.setSize(Vector2f(tokenSize * 7, tokenSize * 6));
+	grid.setFillColor(Color::Yellow);
+	grid.setPosition(Vector2f((window.getSize().x - grid.getSize().x) / 2, (window.getSize().y - grid.getSize().y) / 2)); //Positionne la grille au centre de la fenetre
+
+	token.setRadius(tokenSize / 2);
+	token.setPosition(Vector2f(400 - 32, 32));
+	token.setFillColor(Color::Blue);
+	token.setOutlineColor(Color::Black);
+	token.setOutlineThickness(2);
+
+	while (window.isOpen())
+	{
+		Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::Closed)
+				window.close();
+		}
+		window.clear(Color::Black);
+
+		window.draw(background);
+		window.draw(grid);
+		window.draw(token);
+
+		window.display();
+	}
+
+	return 0;
 }
