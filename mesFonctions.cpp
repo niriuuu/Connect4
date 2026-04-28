a6169b9d4c2ec68525fc37cf3747458e65fa20cc
niriu <oroke3@gmail.com>
2026-04-28 10:36:19 -0400

    stats menu

--- mesFonctions.cpp
+++ mesFonctions.cpp
@@ -1,5 +1,7 @@
 #include <SFML/Graphics.hpp>
+#include <string>
 #include "mesFonctions.h"
+#include "Button.h"
 
 using namespace sf;
 using namespace std;
@@ -9,12 +11,24 @@ void showStats(sf::RenderWindow& window)
 	RectangleShape background;
 	Color backgroundColor(Uint8(35), Uint8(75), Uint8(20), Uint8(255));
 
+	Button exitButton("X", Vector2f(40, 40), Vector2f(10, 10), Color::Black);
+
 	Text text;
 	Font font;
 
 	string content;
 
-	content = string("a") + "b";
+	int gamesPlayed = 0;
+	int p1Wins = 0;
+	int p2Wins = 0;
+	int draws = 0;
+
+	bool goBack = false;
+
+	content = string("Games played: ") + to_string(gamesPlayed)
+		+ "\nPlayer 1 wins: " + to_string(p1Wins)
+		+ "\nPlayer 2 wins: " + to_string(p2Wins)
+		+"\nDraws: " + to_string(draws);
 
 	if (!font.loadFromFile("angelina.ttf"))
 		exit(1);
@@ -22,32 +36,56 @@ void showStats(sf::RenderWindow& window)
 	text.setString(content);
 	text.setCharacterSize(30);
 	text.setFillColor(Color::White);
+	text.setPosition((window.getSize().x - text.getGlobalBounds().width) / 2, (window.getSize().y - text.getGlobalBounds().height) / 2);
 
 	background.setSize(Vector2f(800, 600));
 	background.setFillColor(backgroundColor);
 
-	while (window.isOpen())
+	while (window.isOpen() && !goBack)
 	{
 		Event event;
 		while (window.pollEvent(event))
 		{
 			switch (event.type)
 			{
-			case Event::Closed:
-			{
-				window.close();
-				break;
-			}
-			default:
-				break;
-			}
+				case Event::Closed:
+				{
+					window.close();
+					break;
+				}
+				case Event::MouseMoved:
+				{
+					Vector2i mousePos = Mouse::getPosition(window);
+					sf::Vector2f mousePosF(static_cast<float>(mousePos.x), static_cast<float>(mousePos.y));
+					if (exitButton.isHovered(mousePosF))
+					{
+						exitButton.highlight();
+					}
+					else
+					{
+						exitButton.resetColor();
+					}
+					break;
+				}
+				case Event::MouseButtonPressed:
+				{
+					if (exitButton.getActive())
+					{
+						goBack = true;
+					}
+					break;
+				}
+				default:
+					break;
+				}
 
-			window.clear(Color::Black);
+				window.clear(Color::Black);
 
-			window.draw(background);
-			window.draw(text);
+				window.draw(background);
+				exitButton.draw(window);
+				window.draw(text);
 
-			window.display();
+				window.display();
 		}
 	}
 }
